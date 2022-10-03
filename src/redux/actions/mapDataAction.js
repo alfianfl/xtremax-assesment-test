d63590569a99a3b636b6770b19b6da14af0dca47
alfianfl <alvian.wadada@gmail.com>
2022-10-03 14:47:28 +0700

    add zoom view for selected map

--- src/redux/actions/mapDataAction.js
+++ src/redux/actions/mapDataAction.js
@@ -2,12 +2,15 @@ export const fetchMapData = () => {
   return async (dispatch) => {
     dispatch(fetchMapDataRequest());
     try {
-      // get data from json server
-      const res = await fetch('http://localhost:8000/data', {
+      // get data from github server
+      const res = await fetch('https://raw.githubusercontent.com/alfianfl/xtremax-assesment-test/master/data/mapData.json', {
         method: 'get',
         headers: {
           Accept: 'application/json',
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
+          headers: {
+            'User-Agent': 'request'
+          }
         }
       });
       if (!res.ok) {
